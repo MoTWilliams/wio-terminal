dbb6c2b5b6ed10c16a86c23f0d5d00944c3d5a4b
Mo <MoTaylorWilliams@gmail.com>
2026-04-20 15:13:14 -0500

    moved interrupt handling into command module

--- SerialAndInterrupts/command.h
+++ SerialAndInterrupts/command.h
@@ -1,11 +1,19 @@
+/*
+  Morgan Williams
+  CSCE 3612.001
+  Lab 4: Wio Serial and Interrupts - command.h
+  Apr 22, 2026
+*/
 #ifndef COMMAND_H
 #define COMMAND_H
 
 typedef struct Button Button;
 
-bool executing();
-void executeButtonCommand(Button* b);
-void receiveAndStartSerialCommand();
-void mindSerialCommand();
+void requestAccel();                    // Accelerometer button-press ISR
+void requestTemp();                     // Temperature button-press ISR
+
+void buttonCommand_execute(Button* b);  // Execute button-triggered sensor req.
+void serialCommand_execute();           // Validate and start serial command
+void serialCommand_update();            // Continues timed serial command
 
 #endif
