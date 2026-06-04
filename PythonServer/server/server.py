6816eb8e021bd77ee4e2c1d952ea7445131d6fa1
Mo <MoTaylorWilliams@gmail.com>
2026-06-04 02:27:36 -0500

    Bare minimum server functionality. Great success

--- PythonServer/server/server.py
+++ PythonServer/server/server.py
@@ -1,1 +1,21 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("GET request: Path =", self.path, "; Address =", self.client_address)
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print("Starting httpd...")
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    httpd.server_close()
+    print("Stopping httpd...")
+
+
+run()
